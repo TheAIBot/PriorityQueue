aabd671bebbffb38905d0edbe6f63a9386082d22
TheAIBot <andreasuranus@hotmail.com>
2020-01-31 23:43:14 +0100

    initial commit

---
+++ PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PriorityQueue
+{
+    public class PriorityQueue<T, TPriority>
+    {
+        private ValuePriority<T, TPriority>[] Heap = new ValuePriority<T, TPriority>[1];
+        private readonly IComparer<TPriority> ValueComparar;
+        public int Count { get; private set; } = 0;
+
+        public PriorityQueue() : this(Comparer<TPriority>.Default)
+        {
+        }
+
+        public PriorityQueue(IComparer<TPriority> comparer)
+        {
+            ValueComparar = comparer;
+        }
+
+        public void Enqueue(T t, TPriority priority)
+        {
+            if (Count == Heap.Length)
+            {
+                ExpandHeap();
+            }
+
+            Heap[Count] = new ValuePriority<T, TPriority>(t, priority);
+            BubbleUp(Count);
+            Count++;
+        }
+
+        public T Peek()
+        {
+            return PeekWithPriority().Value;
+        }
+
+        public TPriority PeekPriority()
+        {
+            return PeekWithPriority().Priority;
+        }
+
+        public ValuePriority<T, TPriority> PeekWithPriority()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The priority queue is empty");
+            }
+
+            return Heap[0];
+        }
+
+        public T Dequeue()
+        {
+            return DequeueWithPriority().Value;
+        }
+
+        public ValuePriority<T, TPriority> DequeueWithPriority()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The priority queue is empty");
+            }
+
+            var top = Heap[0];
+            Heap[0] = Heap[Count - 1];
+            Count--;
+            BubbleDown(0);
+
+            return top;
+        }
+
+        private void ExpandHeap()
+        {
+            Array.Resize(ref Heap, Count * 2 + 1);
+        }
+
+        private void BubbleUp(int childIndex)
+        {
+            while (!IsRoot(childIndex))
+            {
+                int parentIndex = Parent(childIndex);
+                var child = Heap[childIndex];
+                var parent = Heap[parentIndex];
+                if (ValueComparar.Compare(child.Priority, parent.Priority) >= 0)
+                {
+                    break;
+                }
+
+                Heap[parentIndex] = child;
+                Heap[childIndex] = parent;
+                childIndex = parentIndex;
+            }
+        }
+
+        private void BubbleDown(int parentIndex)
+        {
+            while (!IsLeaf(parentIndex))
+            {
+                int leftChildIndex = LeftChild(parentIndex);
+                int rightChildIndex = RightChild(parentIndex);
+                if (leftChildIndex >= Count)
+                {
+                    break;
+                }
+
+                int bestChildIndex = leftChildIndex;
+                //If both childs are valid then the best child
+                //between the two will be chosen. Otherwise the
+                //old valid child will be the best child.
+                if (rightChildIndex < Count)
+                {
+                    var leftChild = Heap[leftChildIndex];
+                    var rightChild = Heap[rightChildIndex];
+
+                    if (ValueComparar.Compare(leftChild.Priority, rightChild.Priority) > 0)
+                    {
+                        bestChildIndex = rightChildIndex;
+                    }
+                }
+
+                var bestChild = Heap[bestChildIndex];
+                var parent = Heap[parentIndex];
+                if (ValueComparar.Compare(parent.Priority, bestChild.Priority) <= 0)
+                {
+                    break;
+                }
+
+                Heap[parentIndex] = bestChild;
+                Heap[bestChildIndex] = parent;
+                parentIndex = bestChildIndex;
+            }
+        }
+
+        private int LeftChild(int index)
+        {
+            return (2 * index) + 1;
+        }
+
+        private int RightChild(int index)
+        {
+            return (2 * index) + 2;
+        }
+
+        private int Parent(int index)
+        {
+            return (index - 1) / 2;
+        }
+
+        private bool IsLeaf(int index)
+        {
+            return (index >= Count / 2) && (index < Count);
+        }
+
+        private bool IsRoot(int index)
+        {
+            return index == 0;
+        }
+    }
+}
---
+++ Tests/PriorityQueueTests.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PriorityQueue;
+using System;
+using System.Collections.Generic;
+
+namespace Testing
+{
+    [TestClass]
+    public class PriorityQueueTests
+    {
+
+        [TestMethod]
+        public void EnqueueDequeueIntInt()
+        {
+            var rando = new Random(1);
+            var max = 10000;
+            var queue = new PriorityQueue<int, int>();
+            var numbers = new List<int>();
+
+            for (int i = 0; i < max; i++)
+            {
+                int value = rando.Next(max);
+                numbers.Add(value);
+                queue.Enqueue(value, value);
+            }
+
+            Assert.IsTrue(queue.Count == max);
+
+            while (queue.Count > 0)
+            {
+                var next = queue.Dequeue();
+                Assert.IsTrue(numbers.Contains(next));
+                numbers.Remove(next);
+
+                if (queue.Count > 0)
+                {
+                    Assert.IsTrue(queue.Peek() >= next, $"Expected {queue.Peek().ToString("N0")} to be more than {next.ToString("N0")}.");
+                }
+            }
+
+            Assert.AreEqual(0, numbers.Count);
+        }
+
+        [TestMethod]
+        public void EnqueueDequeueUnorderedIntInt()
+        {
+            var rando = new Random(2);
+            var queue = new PriorityQueue<int, int>();
+            var numbers = new List<int>();
+
+            for (int z = 0; z < 100; z++)
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    int value = rando.Next(int.MinValue, int.MaxValue);
+                    numbers.Add(value);
+                    queue.Enqueue(value, value);
+                }
+                for (int x = 0; x < 300; x++)
+                {
+                    var next = queue.Dequeue();
+                    Assert.IsTrue(numbers.Contains(next));
+                    numbers.Remove(next);
+
+                    if (queue.Count > 0)
+                    {
+                        Assert.IsTrue(queue.Peek() >= next, $"Expected {queue.Peek().ToString("N0")} to be more than {next.ToString("N0")}.");
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void EnqueueDequeueStringInt()
+        {
+            var queue = new PriorityQueue<string, int>();
+
+            queue.Enqueue("A", 2);
+            queue.Enqueue("B", 1);
+            queue.Enqueue("C", 3);
+
+            Assert.AreEqual("B", queue.Dequeue());
+            Assert.AreEqual("A", queue.Dequeue());
+            Assert.AreEqual("C", queue.Dequeue());
+        }
+
+        [TestMethod]
+        public void EnqueueDequeueUnorderedStringInt()
+        {
+            var queue = new PriorityQueue<string, int>();
+
+            queue.Enqueue("A", 2);
+            Assert.AreEqual("A", queue.Peek());
+            Assert.AreEqual("A", queue.Dequeue());
+
+            queue.Enqueue("A", 2);
+            queue.Enqueue("B", 1);
+            Assert.AreEqual("B", queue.Dequeue());
+            Assert.AreEqual("A", queue.Dequeue());
+
+            queue.Enqueue("A", 2);
+            queue.Enqueue("B", 1);
+            queue.Enqueue("C", 3);
+            Assert.AreEqual("B", queue.Dequeue());
+            Assert.AreEqual("A", queue.Dequeue());
+            Assert.AreEqual("C", queue.Dequeue());
+        }
+    }
+}
