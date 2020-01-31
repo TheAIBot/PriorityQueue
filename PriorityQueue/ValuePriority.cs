c80e3e2446e6b70d0b2372091f792457d9e5d6dd
TheAIBot <andreasuranus@hotmail.com>
2020-01-31 23:59:09 +0100

    cleanup usings

--- PriorityQueue/ValuePriority.cs
+++ PriorityQueue/ValuePriority.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PriorityQueue
+﻿namespace PriorityQueue
 {
     public readonly struct ValuePriority<T, TPriority>
     {
